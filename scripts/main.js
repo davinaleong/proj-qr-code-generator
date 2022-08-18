1e867564453461f3010bd2692b8b5cebedeba88a
Davina Leong <davina.leong.sy@gmail.com>
2022-08-18 17:56:05 +0800

    Added submit

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
-console.log(`Script loaded`)
+const formEl = document.querySelector(`[data-form]`)
+const outputEl = document.querySelector(`[data-output]`)
+
+let url = ``
+let size = ``
+let fgColor = ``
+let bgColor = ``
+
+formEl.addEventListener(`submit`, (event) => {
+    event.preventDefault()
+    console.log(formEl.elements)
+})
