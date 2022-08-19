6188116d93ae84bdadf067e4c64242ce395fafc7
Davina Leong <davina.leong.sy@gmail.com>
2022-08-19 10:01:09 +0800

    Able to generate a QR Code

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,15 @@
 const formEl = document.querySelector(`[data-form]`)
 const outputEl = document.querySelector(`[data-output]`)
 
+const renderOutput = (html) => {
+  outputEl.innerHTML = html
+}
+
 formEl.addEventListener(`submit`, (event) => {
   event.preventDefault()
-  outputEl.innerHTML = `
-        <i class="fa fa-spinner fa-spin fa-10x text-primary-400"></i>
-    `
+  renderOutput(`
+    <i class="fa fa-spinner fa-spin fa-10x text-primary-400"></i>
+  `)
   const url = formEl.elements[`url`].value
   const size = formEl.elements[`size`].value
   const fgColor = formEl.elements[`fg-color`].value
@@ -21,4 +25,46 @@ formEl.addEventListener(`submit`, (event) => {
     `bgColor: `,
     bgColor
   )
+
+  let errors = ``
+
+  if (!url) {
+    errors += `
+        <p class="text-danger-400">The <strong>URL</strong> field cannot be empty!</p>
+    `
+  }
+
+  if (!size) {
+    errors += `
+        <p class="text-danger-400">The <strong>Size in pixels</strong> field cannot be empty!</p>
+    `
+  }
+
+  if (!fgColor) {
+    errors += `
+        <p class="text-danger-400">The <strong>Foreground Color</strong> field cannot be empty!</p>
+    `
+  }
+
+  if (!bgColor) {
+    errors += `
+        <p class="text-danger-400">The <strong>Background Color</strong> field cannot be empty!</p>
+    `
+  }
+
+  if (errors) {
+    renderOutput(errors)
+  }
+
+  if (!errors) {
+    renderOutput(``)
+    new QRCode(outputEl, {
+      text: url,
+      width: size,
+      height: size,
+      colorDark: fgColor,
+      colorLight: bgColor,
+      correctLevel: QRCode.CorrectLevel.H,
+    })
+  }
 })
